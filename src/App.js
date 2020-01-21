5a23dfe5b0542a06cb3b8de9588759ed0bfb3e20
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-22 07:48:21 +0900

    Characters

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-
+import Validation from "./Components/Validation/Validation";
+import Char from "./Components/Char/Char";
 export class App extends Component {
   state = {
     userInput: ""
   };
+
   inputChangeHandler = event => {
     this.setState({ userInput: event.target.value });
   };
+
   render() {
+    let charList = this.state.userInput.split("").map(ch => {
+      return <Char character={ch} />;
+    });
     return (
       <div>
         <h1>Type something</h1>
@@ -17,6 +23,8 @@ export class App extends Component {
           value={this.state.userInput}
         />
         <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
     );
   }
---
+++ src/Components/Char/Char.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Char = props => {
+    const style = {
+        display: 'inline-block',
+        padding: '16px',
+        margin: '16px',
+        border: '1px solid black',
+        textAlign: 'center'
+    };
+
+    return (
+        <div style={style}>
+        {props.character}
+        </div>
+    )
+}
+
+export default Char;
